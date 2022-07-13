0ba6ed47ee492fb3f16c217805a2913ea9d862d3
syp95 <sy95_055@naver.com>
2022-07-13 20:47:44 +0900

    Delete, Update Plant Data

--- components/Plant.tsx
+++ components/Plant.tsx
@@ -1,22 +1,107 @@
-import { deleteDoc, doc } from 'firebase/firestore';
+import { deleteDoc, doc, updateDoc } from 'firebase/firestore';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useRecoilState } from 'recoil';
+import styled from 'styled-components';
+import { numberPickerState, pickNumberState } from '../atoms/atoms';
 import { fbDb } from '../firebase/firebase';
+import NumberPicker from './NumberPicker';
+
+const OpenPicker = styled(motion.div)`
+    width: 520px;
+    position: absolute;
+    bottom: 0;
+`;
 
 const Plant = ({ plantData }: any) => {
+    const [editing, setEditing] = useState(false);
+    const [newPlantWater, setNewPlantWater] = useState(plantData.wateringDate);
+    const [newPlantName, setNewPlantName] = useState(plantData.plantName);
+    const PlantRef = doc(fbDb, 'plant', plantData.id);
+
     const onDelete = async () => {
         const ok = window.confirm('정말 지우실건가요?');
         if (ok) {
             console.log(plantData.id);
-            await deleteDoc(doc(fbDb, 'plant', plantData.id));
+            await deleteDoc(PlantRef);
         }
     };
 
+    const toggleEditing = () => {
+        setEditing((prev) => !prev);
+        setPick(0);
+    };
+    const onChange = (e: any) => {
+        const {
+            target: { value },
+        } = e;
+        setNewPlantName(value);
+    };
+    const onSubmit = async (data: any) => {
+        await updateDoc(PlantRef, {
+            wateringDate: pick,
+            plantName: data.name,
+        });
+
+        toggleEditing();
+        setNewPlantWater(pick);
+        setPick(0);
+    };
+
+    const [numberPicker, setNumberPicker] = useRecoilState(numberPickerState);
+    const [pick, setPick] = useRecoilState(pickNumberState);
+
+    const onNumberPicker = () => {
+        setNumberPicker(true);
+    };
+
+    const { register, handleSubmit, formState } = useForm();
     return (
         <>
-            <div>{plantData.plantName}</div>
-            <div>{plantData.wateringDate}일 마다 한번씩</div>
-            <div>{plantData.lastWateringDate}</div>
-            <button onClick={onDelete}>delete</button>
-            <button>update</button>
+            {editing ? (
+                <>
+                    <form onSubmit={handleSubmit((data) => onSubmit(data))}>
+                        <span>물 주기</span>
+                        <input
+                            {...register('name')}
+                            value={newPlantName}
+                            onChange={onChange}
+                            type='text'
+                            required
+                        />
+                        <input
+                            autoComplete='off'
+                            onClick={onNumberPicker}
+                            onChange={() => pick}
+                            value={pick ? String(pick) : newPlantWater}
+                            type='text'
+                            required
+                        />
+                        <input type='submit' value='확인' />
+                    </form>
+                    <button onClick={toggleEditing}>취소</button>
+                </>
+            ) : (
+                <>
+                    <div>{plantData.plantName}</div>
+                    <div>{plantData.wateringDate}일 마다 한번씩</div>
+                    <div>{plantData.lastWateringDate}</div>
+                    <button onClick={onDelete}>delete</button>
+                    <button onClick={toggleEditing}>update</button>
+                </>
+            )}
+            <AnimatePresence>
+                {numberPicker && (
+                    <OpenPicker
+                        initial={{ transform: 'translateY(300px)' }}
+                        animate={{ transform: 'translateY(000px)' }}
+                        exit={{ transform: 'translateY(300px)' }}
+                        transition={{ type: 'tween', duration: 0.7 }}>
+                        <NumberPicker />
+                    </OpenPicker>
+                )}
+            </AnimatePresence>
         </>
     );
 };
