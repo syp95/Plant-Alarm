561161bd7b288170d82fce6bf0f66d4f81a42599
syp95 <sy95_055@naver.com>
2022-07-14 22:21:20 +0900

    print mainpage, circulate watering date

--- components/PlantSlider.tsx
+++ components/PlantSlider.tsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from 'react';
+
 const PlantSlider = ({ plantData }: any) => {
+    const [waterRestDay, setWaterRestDay] = useState(0);
+    const [waterRestPer, setWaterRestPer] = useState(0);
+
+    const getDateDiffNow = (lastDate: string, Water: string) => {
+        let today = new Date();
+        let year = today.getFullYear();
+        let month = String(today.getMonth() + 1).padStart(2, '0');
+        let day = String(today.getDate()).padStart(2, '0');
+
+        let dateNow = `${year}-${month}-${day}`;
+        const currentDate = new Date(dateNow);
+        const lastWateringDate = new Date(lastDate);
+        const wateringDate = Number(Water) * (1000 * 60 * 60 * 24);
+
+        const restDay =
+            (lastWateringDate.getTime() +
+                wateringDate -
+                currentDate.getTime()) /
+            (1000 * 60 * 60 * 24);
+        const restPer = (restDay / Number(Water)) * 100;
+
+        setWaterRestDay(restDay);
+        setWaterRestPer(restPer);
+    };
+
+    useEffect(() => {
+        getDateDiffNow(plantData.lastWateringDate, plantData.wateringDate);
+    }, []);
+
     return (
         <>
-            <div>{plantData.wateringDate}</div>
+            <div>{plantData.plantName}</div>
+            <div>물 주기 : {plantData.wateringDate}</div>
+            <div>{waterRestDay} 일 남았습니다.</div>
+            <div>물이 {Math.round(waterRestPer)}% 정도 남아있어요.</div>
         </>
     );
 };
